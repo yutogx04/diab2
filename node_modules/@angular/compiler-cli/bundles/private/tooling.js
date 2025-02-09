b953aabf1bb1d42eb41012f54fc3e6f8a0da2c81
yutogx04 <lehanine.faical@gmail.com>
2025-02-09 06:59:04 -0500

    hi

---
+++ node_modules/@angular/compiler-cli/bundles/private/tooling.js
@@ -0,0 +1,21 @@
+
+      import {createRequire as __cjsCompatRequire} from 'module';
+      const require = __cjsCompatRequire(import.meta.url);
+    
+import {
+  GLOBAL_DEFS_FOR_TERSER,
+  GLOBAL_DEFS_FOR_TERSER_WITH_AOT,
+  constructorParametersDownlevelTransform
+} from "../chunk-C7LUE5H7.js";
+import "../chunk-RFP3I4Z5.js";
+import "../chunk-FTKLQ24G.js";
+import "../chunk-MMGTGRB2.js";
+import "../chunk-Q2WE7ECN.js";
+import "../chunk-37JMVF7H.js";
+import "../chunk-KPQ72R34.js";
+export {
+  GLOBAL_DEFS_FOR_TERSER,
+  GLOBAL_DEFS_FOR_TERSER_WITH_AOT,
+  constructorParametersDownlevelTransform
+};
+//# sourceMappingURL=tooling.js.map
---
+++ node_modules/@angular/compiler-cli/bundles/src/bin/ngc.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+      import {createRequire as __cjsCompatRequire} from 'module';
+      const require = __cjsCompatRequire(import.meta.url);
+    
+import {
+  main
+} from "../../chunk-XJHTEQ76.js";
+import "../../chunk-E53LG5FO.js";
+import "../../chunk-RFP3I4Z5.js";
+import "../../chunk-FTKLQ24G.js";
+import "../../chunk-MMGTGRB2.js";
+import "../../chunk-Q2WE7ECN.js";
+import {
+  NodeJSFileSystem,
+  setFileSystem
+} from "../../chunk-37JMVF7H.js";
+import "../../chunk-KPQ72R34.js";
+
+// bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/bin/ngc.mjs
+import "reflect-metadata";
+async function runNgcComamnd() {
+  process.title = "Angular Compiler (ngc)";
+  const args = process.argv.slice(2);
+  setFileSystem(new NodeJSFileSystem());
+  process.exitCode = main(args, void 0, void 0, void 0, void 0, void 0);
+}
+runNgcComamnd().catch((e) => {
+  console.error(e);
+  process.exitCode = 1;
+});
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+//# sourceMappingURL=ngc.js.map
